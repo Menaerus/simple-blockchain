08658308c7b3c4c76197750633e3affdbd42a64c
David <david@mannes-tech.com>
2024-02-06 16:34:23 +0100

    Enhanced tests with equality

--- tests/tests.py
+++ tests/tests.py
@@ -7,10 +7,22 @@ from blockchain import Blockchain,Block
 class TestBlockchain(unittest.TestCase):
 
   def test_verify_genesis(self):
-    self.assertTrue(Blockchain().is_valid())
+    self.assertTrue(Blockchain(date.datetime.now()).is_valid())
 
   def test_verify_addingblock(self):
-    self.assertTrue(Blockchain().add_block(Block(1, date.datetime.now(), "First testing block", "")).is_valid())
+    self.assertTrue(Blockchain(date.datetime.now()).add_block(Block(1, date.datetime.now(), "First testing block", "")).is_valid())
+
+  def test_equality(self):
+    genesistime = date.datetime.now()
+    b1 = Blockchain(genesistime)
+    dates = [date.datetime.now() for i in range(100)]
+    strings = [f'Equality Testing Block number {number}' for number in range(len(dates))]
+    blocks = [Block(i+1, dates[i], strings[i], "") for i in range(len(dates))]
+    b2 = Blockchain(genesistime)
+    for b in blocks:
+      b1.add_block(b)
+      b2.add_block(b)
+    self.assertEqual(str(b1), str(b2))
 
 if __name__ == '__main__':
     unittest.main()
